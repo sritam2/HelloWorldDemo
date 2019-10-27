3433b132c2e45cf6c1a11e0e3246b3003d9d8eb0
sritam2 <sritam2@gmail.com>
2019-10-28 00:29:28 +0100

    understand merging in Git

--- GitHubHelloWorld2.c
+++ GitHubHelloWorld2.c
@@ -5,6 +5,7 @@ int main()
 {
     printf("Hello World From GitHub - replica \n");
 	printf("Hello World paltasingh dearoo - replica\n");
+	printf("Familiarizing myself with GIT \n");
 	printf("Hello World Puniya dearoo - replica\n");
 	printf("Hello World lipak dearoo - replica\n");
 	printf("Hello World zaffi bhai \n");
