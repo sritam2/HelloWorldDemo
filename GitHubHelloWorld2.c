cf215397b1b3c04da8023dde16a0176154b84b5c
sritam2 <sritam2@gmail.com>
2019-10-28 00:16:31 +0100

    Playing around with Git

--- GitHubHelloWorld2.c
+++ GitHubHelloWorld2.c
@@ -4,7 +4,7 @@
 int main()
 {
     printf("Hello World From GitHub - replica \n");
-	printf("Hello World palta dearoo - replica\n");
+	printf("Hello World paltasingh dearoo - replica\n");
 	printf("Hello World Puniya dearoo - replica\n");
 	printf("Hello World lipak dearoo - replica\n");
 	printf("Hello World zaffi bhai \n");
